bdc0ad61fd5d21d2f2bf98e33ce86d22638f2493
ouboussetta <abdelkrim.ouboussetta@gmail.com>
2020-04-29 17:48:31 +0200

    manqu titre

--- src/Migrations/Version20200429152352.php
+++ src/Migrations/Version20200429152352.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200427133354 extends AbstractMigration
+final class Version20200429152352 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,8 +22,7 @@ final class Version20200427133354 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE document (id INT AUTO_INCREMENT NOT NULL, page_id INT DEFAULT NULL, titre VARCHAR(255) NOT NULL, INDEX IDX_D8698A76C4663E4 (page_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE document ADD CONSTRAINT FK_D8698A76C4663E4 FOREIGN KEY (page_id) REFERENCES page (id)');
+        $this->addSql('DROP TABLE fichier');
     }
 
     public function down(Schema $schema) : void
@@ -31,6 +30,6 @@ final class Version20200427133354 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE document');
+        $this->addSql('CREATE TABLE fichier (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
     }
 }
